99ce45c5882ec763396c70ae6d1c9ee876d2ae67
NAOKO <naomekko.i@gmail.com>
2023-12-13 01:27:39 +0900

    no message

--- js/burger.js
+++ js/burger.js
@@ -1,9 +1,10 @@
 function humburger(){
-    document.querySelector('#line1').classList.toggle('line_1');
-    document.querySelector('#line2').classList.toggle('line_2');
-    document.querySelector('#line3').classList.toggle('line_3');
-    document.querySelector('#navi').classList.toggle('action');
+    document.querySelector('#line1').classList.toggle("line_1");
+    document.querySelector('#line2').classList.toggle("line_2");
+    document.querySelector('#line3').classList.toggle("line_3");
+    document.querySelector('#navi').classList.toggle("action");
 }
-document.querySelector('#burger').addEventListener('click',function(){
+document.querySelector("#burger").addEventListener('click' ,function(){
     humburger();
 });
+
